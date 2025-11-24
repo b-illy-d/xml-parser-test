c0c1a513ecb55375d45cb6509e27ddf991b810d1
Billy D <billy@triplewhale.com>
2025-11-24 01:58:42 -0500

    refactor for ease of adding new sources

--- parser/parse.py
+++ parser/parse.py
@@ -1,11 +1,31 @@
 from __future__ import annotations
 
+import itertools
 from contextlib import suppress
-from typing import List
+from typing import Tuple
 
 from lxml import etree
 
 from .types import StreamLike
+
+
+# These tests receive the <doc-number> element and return True if it matches the source
+def _is_epo(element: etree.Element) -> bool:
+    doc_id = element.getparent()
+    if doc_id is None:
+        return False
+    return doc_id.attrib.get("format") == "epo"
+
+
+def _is_patent_office(element: etree.Element) -> bool:
+    doc_id = element.getparent()
+    if doc_id is None:
+        return False
+    return doc_id.attrib.get("load-source") == "patent-office"
+
+
+# Order of this list determines the order of parser results
+SOURCES_TESTS = [_is_epo, _is_patent_office]
 
 
 def parse_stream(
@@ -21,29 +41,53 @@ def parse_stream(
         huge_tree=True,
     )
 
-    epo_nums: List[str] = []
-    patent_office_nums: List[str] = []
+    doc_numbers_by_source = [[] for _ in SOURCES_TESTS]
+
     try:
         in_application_reference = False
+        in_document_id = False
         for event, element in parser:
-            if event == "end" and element.tag == "document-id":
-                is_epo = element.attrib.get("format") == "epo"
-                is_patent_office = element.attrib.get("load-source") == "patent-office"
-                for child in element:
-                    if child.tag == "doc-number":
-                        if is_epo and child.text is not None:
-                            epo_nums.append(child.text)
-                        elif is_patent_office and child.text is not None:
-                            patent_office_nums.append(child.text)
-                element.clear(keep_tail=True)
-            elif event == "start" and element.tag == "application-reference":
-                in_application_reference = True
-            elif event == "end" and element.tag == "application-reference":
-                in_application_reference = False
-            if event == "start" and not in_application_reference:
-                element.clear(keep_tail=True)
-
+            if event == "start":
+                match element.tag:
+                    case "application-reference":
+                        in_application_reference = True
+                    case "document-id" if in_application_reference:
+                        in_document_id = True
+            elif event == "end":
+                match element.tag:
+                    case "application-reference":
+                        in_application_reference = False
+                    case "document-id":
+                        in_document_id = False
+                    case "doc-number" if in_document_id:
+                        index, value = _handle(element)
+                        if index >= 0:
+                            doc_numbers_by_source[index].append(value)
+                        element.clear(keep_tail=True)
+                    case _:
+                        element.clear(keep_tail=True)
     finally:
         with suppress(Exception):
             source.close()
-    return epo_nums + patent_office_nums
+    return _flatten(doc_numbers_by_source)
+
+
+def _flatten(
+    lists: list[list[str]],
+) -> list[str]:
+    return list(
+        itertools.chain.from_iterable(
+            lists,
+        ),
+    )
+
+
+def _handle(
+    element: etree.Element,
+) -> Tuple[int, str]:
+    if element.text is None:
+        return -1, ""
+    for index, test in enumerate(SOURCES_TESTS):
+        if test(element):
+            return index, element.text or ""
+    return -1, ""  # if it's not from one of our sources we can ignore it
